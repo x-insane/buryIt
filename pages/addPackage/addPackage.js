6a1f322f61c31feeec19dd486463a8f73bfa16a8
coomrade <842490183@qq.com>
2018-04-20 23:22:06 +0800

    18.4.20 23:22

--- pages/addPackage/addPackage.js
+++ pages/addPackage/addPackage.js
@@ -1,66 +1,22 @@
 // pages/addPackage/addPackage.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  data:{
+    packageTypes:[
+      {
+      name:"package1",
+      value:"包裹type1",
+    },
+    {
+      name:"package2",
+      value:"包裹type2",
+    },
+    {
+      name:"package3",
+      value:"包裹type3"
+    }],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+//选中项发生变化时
+choiceChanged:function(e){
+  console.log(e.detail.value);
+}
 })
